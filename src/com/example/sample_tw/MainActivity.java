9e53011ee62358f5d7aa9d2b62d78f4a91b61185
yuigotou <s3011065@surugadai.ac.jp>
2015-01-18 22:57:29 +0900

    試行錯誤

--- src/com/example/sample_tw/MainActivity.java
+++ src/com/example/sample_tw/MainActivity.java
@@ -184,8 +184,12 @@ public class MainActivity extends Activity {
 
 		case R.id.item2:
 			Log.d("", "認証が押されたよ〜");
-			// OAuth認証のあとコールバック
-			TwitterLoginOAuth();
+			// PINコード認証のあとコールバック
+			
+			final int REQUEST_ACCESS_TOKEN = 0;
+			Intent intent2 = new Intent(MainActivity.this,
+					TwitterLoginOAuth.class);
+			startActivity(intent2);	
 
 		case R.id.item3:
 			Log.d("", "とじるが押されたよ〜");
--- src/com/example/sample_tw/TwitterLoginOAuth.java
+++ src/com/example/sample_tw/TwitterLoginOAuth.java
@@ -12,78 +12,78 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.EditText;
 
 public class TwitterLoginOAuth extends Activity implements OnClickListener {
-    public final static String EXTRA_CONSUMER_KEY = "SjaTsufLGmFsVyFCwfA28eekb";
-    public final static String EXTRA_CONSUMER_SECRET = "5fF7B1eQSBxnfw7gVbLM33swhaJ6AXkDrs8pUYE4qboDK3K4i1";
-    public final static String EXTRA_ACCESS_TOKEN = "601867423-g9Xh2jV8ipU0MLBrm8H0UurpTewLJnDMOWbPs34t";
-    public final static String EXTRA_ACCESS_TOKEN_SECRET = "7FD6q9dKjr909WGV0EMdq7rOdalgFBlrlbrd11eFBs5zn";
+	public final static String EXTRA_CONSUMER_KEY = "SjaTsufLGmFsVyFCwfA28eekb";
+	public final static String EXTRA_CONSUMER_SECRET = "5fF7B1eQSBxnfw7gVbLM33swhaJ6AXkDrs8pUYE4qboDK3K4i1";
+	public final static String EXTRA_ACCESS_TOKEN = "601867423-g9Xh2jV8ipU0MLBrm8H0UurpTewLJnDMOWbPs34t";
+	public final static String EXTRA_ACCESS_TOKEN_SECRET = "7FD6q9dKjr909WGV0EMdq7rOdalgFBlrlbrd11eFBs5zn";
 
-    private RequestToken mRequestToken;
-    final AsyncTwitterFactory factory = new AsyncTwitterFactory();
-    final AsyncTwitter twitter = factory.getInstance();
+	private RequestToken mRequestToken;
+	final AsyncTwitterFactory factory = new AsyncTwitterFactory();
+	final AsyncTwitter twitter = factory.getInstance();
 
-    // 非同期版 Twitter4J のリスナ
-    private final TwitterListener listener = new TwitterAdapter() {
-            @Override
-            public void gotOAuthRequestToken(RequestToken token) {
-                mRequestToken = token;
-            }
+	// 非同期版 Twitter4J のリスナ
+	private final TwitterListener listener = new TwitterAdapter() {
+		@Override
+		public void gotOAuthRequestToken(RequestToken token) {
+			mRequestToken = token;
+		}
 
-            @Override
-            public void gotOAuthAccessToken(AccessToken token) {
-                // Access Token 取得成功
-                // 呼び出し元に Access Token を返す
-                final Intent intent = new Intent();
-                intent.putExtra(EXTRA_ACCESS_TOKEN, token.getToken());
-                intent.putExtra(EXTRA_ACCESS_TOKEN_SECRET, token.getTokenSecret());
-                setResult(Activity.RESULT_OK, intent);
-                finish();
-            }
-        };
+		@Override
+		public void gotOAuthAccessToken(AccessToken token) {
+			// Access Token 取得成功
+			// 呼び出し元に Access Token を返す
+			final Intent intent = new Intent();
+			intent.putExtra(EXTRA_ACCESS_TOKEN, token.getToken());
+			intent.putExtra(EXTRA_ACCESS_TOKEN_SECRET, token.getTokenSecret());
+			setResult(Activity.RESULT_OK, intent);
+			finish();
+		}
+	};
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_oauth);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_oauth);
 
-        // Request Token をリクエスト
-        final Intent intent = getIntent();
-        final String consumer_key = intent.getStringExtra(EXTRA_CONSUMER_KEY);
-        final String consumer_secret = intent.getStringExtra(EXTRA_CONSUMER_SECRET);
-        twitter.addListener(listener);
-        twitter.setOAuthConsumer(consumer_key, consumer_secret);
-        twitter.getOAuthRequestTokenAsync();
+		// Request Token をリクエスト
+		final Intent intent = getIntent();
+		final String consumer_key = EXTRA_CONSUMER_KEY;
+		final String consumer_secret = EXTRA_CONSUMER_SECRET;
+		twitter.addListener(listener);
+		// twitter.setOAuthConsumer(consumer_key, consumer_secret);
+		twitter.getOAuthRequestTokenAsync();
 
-        // EventListener をセット
-        final View start_login = findViewById(R.id.button_start_login);
-        start_login.setOnClickListener(this);
-        final View login = findViewById(R.id.button_login);
-        login.setOnClickListener(this);
-    }
+		// EventListener をセット
+		final View start_login = findViewById(R.id.button_start_login);
+		start_login.setOnClickListener(this);
+		final View login = findViewById(R.id.button_login);
+		login.setOnClickListener(this);
+	}
 
-    @Override
-    public void onClick(View v) {
-        switch(v.getId()) {
-        case R.id.button_start_login:
-            {
-                // 認証画面をブラウザで開く
-                final Intent intent = new Intent(Intent.ACTION_VIEW,
-                                                 Uri.parse(mRequestToken.getAuthorizationURL()));
-                startActivity(intent);
-            }
-            break;
-        case R.id.button_login:
-            {
-                // PINコードを取得
-                final String pin = editPin.getText().toString();
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.button_start_login: {
+			// 認証画面をブラウザで開く
+			final Intent intent = new Intent(Intent.ACTION_VIEW,
+					Uri.parse(mRequestToken.getAuthorizationURL()));
+			startActivity(intent);
+		}
+			break;
+		case R.id.button_login: {
+			EditText editPin = (EditText) findViewById(R.id.edit_pin_code);
+			// PINコードを取得
+			final String pin = editPin.getText().toString();
 
-                // Access Token をリクエスト
-                twitter.getOAuthAccessTokenAsync(mRequestToken, pin);
-            }
-            break;
-        }
-    }
+			// Access Token をリクエスト
+			twitter.getOAuthAccessTokenAsync(mRequestToken, pin);
+		}
+			break;
+		}
+	}
 }
