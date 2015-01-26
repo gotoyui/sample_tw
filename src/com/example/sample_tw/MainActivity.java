125aa251477ec809103ebeee5ce7498623c93b64
gotoyui <s3011065@surugadai.ac.jp>
2015-01-26 10:48:41 +0900

    PIN認証でツイッターにアクセス

--- src/com/example/sample_tw/MainActivity.java
+++ src/com/example/sample_tw/MainActivity.java
@@ -184,11 +184,10 @@ public class MainActivity extends Activity {
 
 		case R.id.item2:
 			Log.d("", "認証が押されたよ〜");
-			// PINコード認証のあとコールバック まだ未設定　むずかしい・・・！
-			//final int REQUEST_ACCESS_TOKEN = 0;
-			//Intent intent2 = new Intent(MainActivity.this,
-			//		TwitterLoginPIN.class);
-			//startActivity(intent2);
+			// PINコード認証のあとコールバック
+			Intent intent2 = new Intent(MainActivity.this,
+					TwitterLoginPIN.class);
+			startActivity(intent2);
 
 		case R.id.item3:
 			Log.d("", "とじるが押されたよ〜");
